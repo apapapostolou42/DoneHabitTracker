c174ce1cd553620c61ed3f3e3b6c559a758c9be4
Oblivious <thanos.papap42@gmail.com>
2023-12-12 13:04:29 +0200

    Initial Commit

---
+++ DoneHabitTracker/DoneHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoneHabitTrackerApp.swift
+//  DoneHabitTracker
+//
+//  Created by Papapostolou, Athanasios on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoneHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
