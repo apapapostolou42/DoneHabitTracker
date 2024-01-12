4bc35a84e685be699e49cdd911b620bd9f2f4521
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2024-01-12 15:48:26 +0200

    Added Bottom Icons

--- DoneHabitTracker/Scenes/MainView.swift
+++ DoneHabitTracker/Scenes/MainView.swift
@@ -11,25 +11,58 @@ import FirebaseAuth
 struct MainView: View {
     
     @Binding var isLoading: Bool
+    @State private var selectedTab = 0
     
     var body: some View {
         
         ZStack {
-            TabView {
+            TabView(selection: $selectedTab) {
+                
                 ProfileView(isLoading: $isLoading)
-                    .tabItem {
-                        Label("Home", systemImage: "house")
-                    }
                 
-                Text("Search Tab")
                     .tabItem {
-                        Label("Search", systemImage: "magnifyingglass")
+                        Image("ti_profile")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 0 ? .blue : .gray)
+                        Text("Profile")
                     }
+                    .tag(0)
                 
-                Text("Profile Tab")
+                Text("Home Tab")
                     .tabItem {
-                        Label("Profile", systemImage: "person")
+                        Image("ti_home")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 0 ? .blue : .gray)
+                        Text("Home")
                     }
+                    .tag(1)
+                
+                Text("Statistics Tab")
+                    .tabItem {
+                        Image("ti_statistics")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 0 ? .blue : .gray)
+                        Text("Statistics")
+                    }
+                    .tag(2)
+                
+                Text("Star Tab")
+                    .tabItem {
+                        Image("ti_star")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 0 ? .blue : .gray)
+                        Text("Star")
+                    }
+                    .tag(3)
+                
+                Text("Plus Tab")
+                    .tabItem {
+                        Image("ti_plus")
+                            .renderingMode(.template)
+                            .foregroundColor(selectedTab == 0 ? .blue : .gray)
+                        Text("Plus")
+                    }
+                    .tag(4)
             }
         }
     }
