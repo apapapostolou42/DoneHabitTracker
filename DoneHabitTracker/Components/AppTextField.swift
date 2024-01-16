c9c4e451f0361169aa2db601203c52cc05c0add3
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2024-01-17 01:21:42 +0200

    Allow for SecureField implementation in AppTextField UI Component

--- DoneHabitTracker/Components/AppTextField.swift
+++ DoneHabitTracker/Components/AppTextField.swift
@@ -13,12 +13,14 @@ struct AppTextField: View {
     private let borderColor: Color
     private let borderWidth: CGFloat
     private let cornerRadius: CGFloat
+    private let secured: Bool
     
     @Environment(\.colorScheme) var colorScheme
     
-    init(hint: String = "", text: Binding<String>, borderColor: Color = .secondary, borderWidth: CGFloat = 2.0, cornerRadius: CGFloat = 5.0) {
+    init(hint: String = "", text: Binding<String>, secured: Bool = false, borderColor: Color = .secondary, borderWidth: CGFloat = 2.0, cornerRadius: CGFloat = 5.0) {
         self.hint = hint
         self._text = text
+        self.secured = secured
         self.borderColor = borderColor
         self.borderWidth = borderWidth
         self.cornerRadius = cornerRadius
@@ -31,10 +33,18 @@ struct AppTextField: View {
                 .background(RoundedRectangle(cornerRadius: 8).fill(colorScheme == .dark ? .black : .white))
                 .frame(height: 40 - borderWidth * 2)
             
-            TextField(hint.localized, text: $text)
-                .padding(.horizontal, 8)
-                .padding(.vertical, borderWidth)
-                .foregroundColor(.primary)
+            if secured {
+                SecureField(hint.localized, text: $text)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, borderWidth)
+                    .foregroundColor(.primary)
+            }
+            else {
+                TextField(hint.localized, text: $text)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, borderWidth)
+                    .foregroundColor(.primary)
+            }
         }
         .frame(maxWidth: .infinity)
         .padding(.horizontal, borderWidth)
@@ -47,6 +57,8 @@ struct AppTextField: View {
         AppTextField(hint: "Enter Value", text: .constant(""), borderColor: .secondary, borderWidth: 2.0)
         
         AppTextField(hint: "Enter Value", text: .constant("xxx"), borderColor: .secondary, borderWidth: 2.0)
+        
+        AppTextField(hint: "Enter Value", text: .constant("xxx"), secured: true, borderColor: .secondary, borderWidth: 2.0)
     }
     .padding(32)
 }
